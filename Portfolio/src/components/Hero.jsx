edd6510555d5a71f3098aa9a7b8649ea9bcb6a16
Himanshityagii24 <tyagihimanshi241@gmail.com>
2025-01-05 20:25:31 +0530

    tailwind config

--- Portfolio/src/components/Hero.jsx
+++ Portfolio/src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hello!<span className='text-[#129eaf]'>Himanshi this side</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a Mern/Next.js Web developer <br className='sm:block hidden' />
+            I am a  Software developer <br className='sm:block hidden' />
             
           </p>
         </div>
