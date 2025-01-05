812b1e0f70531c26141a9098edee11ef2186e49c
Himanshityagii24 <tyagihimanshi241@gmail.com>
2025-01-06 03:55:01 +0530

    error fixing

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,33 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Hero, Navbar, Tech, Works } from "./components";
+import { About, Contact, Experience,  Hero, Navbar, Tech, Works } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar/>
           <Hero />
         </div>
-        <div id="about">
-          <About />
-        </div>
-        <div id="experience">
-          <Experience />
-        </div>
-        <div id="tech">
-          <Tech />
-        </div>
-        <div id="works">
-          <Works />
-        </div>
-        <div id="contact">
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+       
           <Contact />
-        </div>
+         
+  
+       
       </div>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
